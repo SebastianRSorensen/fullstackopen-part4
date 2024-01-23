7fbadbe83de454be1f336389d7a4f4b061b53e44
Sebastian Rosnes Sørensen <133763422+SebastianRSorensen@users.noreply.github.com>
2024-01-23 16:09:22 +0100

    done 4.1

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+console.log(process.argv);
+const password = process.argv[2];
+const mongoUrl = `mongodb+srv://sebastianrosnessorensen:${password}@cluster0-blog.svo1zpy.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(mongoUrl).then(() => {
+  console.log("connected to MongoDB");
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
